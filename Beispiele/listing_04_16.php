9f216f2ce72e4c459e60271f252bd33627dada6e
Nisc3d <nisc3c@protonmail.com>
2019-09-03 14:01:20 +0200

    Neue Beispiele

---
+++ Beispiele/listing_04_16.php
@@ -0,0 +1,8 @@
+<?php
+$anfang = 1;
+$ende = 10;
+while ($anfang <= $ende) {
+    echo $anfang," ";
+    $anfang++;
+}
+?>
---
+++ Beispiele/listing_04_34.php
@@ -0,0 +1,8 @@
+<?php
+for ($reihe = 1; $reihe <= 10; $reihe++) {
+    for ($spalte = 1; $spalte <= 10; $spalte++) {
+        echo $reihe * $spalte," ";
+    }
+    echo "<br>";
+}
+?>
