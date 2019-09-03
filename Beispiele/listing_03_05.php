7ddeecbfe8dade542ee75623ce4bdf1aca03af9a
Nisc3d <nisc3c@protonmail.com>
2019-09-03 14:14:50 +0200

    Neue Beispiele

---
+++ Beispiele/listing_03_05.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<?php
+$nachname = $_POST["name"];
+$vorname = $_POST["vorname"];
+$name = $vorname." ".$nachname;
+?>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Auswertung</title>
+</head>
+<body>
+<p>
+    Guten Tag, <?= $name?>!
+</p>
+</body>
+</html>
