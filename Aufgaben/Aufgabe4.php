15d56b21ec06dd2e0008335160ebe949c2697e71
Nisc3d <nisc3d@protonmail.com>
2019-09-05 18:44:08 +0200

    Update

--- Aufgaben/Aufgabe4.php
+++ Aufgaben/Aufgabe4.php
@@ -6,28 +6,30 @@
 <body>
 <h2>Pizzabestellung:</h2>
 <form action="Aufgabe4_Ausgabe.php" method="post">
-    <p>Name:<input type="text" name="Name"></p>
-    <p>Adresse:<input type="text" name="Adresse"></p>
-    <p>Geschlecht:<input type="text" name="Geschlecht"></p>
+    <p>Name:<input type="text" name="Name:"></p>
+    <p>Adresse:<input type="text" name="Adresse:"></p>
+    <p>Geschlecht:<input type="text" name="Geschlecht:"></p>
     <br>
 
     <h4>Pizza auswählen:</h4>
-    <p><select multiple="Pizzen" name="Pizza[]">
-            <option value="Pizza Salami 5">Pizza Salami 5€</option>
-            <option value="Pizza Schinken 5">Pizza Schinken 5€</option>
-            <option value="Pizza Hawaii 6">Pizza Hawaii 6€</option>
-            <option value="Pizza SchinkenPilze 6">Pizza Schinken und Pilze 6€</option>
-            <option value="Pizza Margherita 4">Pizza Margeherita 4€</option>
+    <p><select multiple="multiple" name="Pizza[]">
+            <option value="PizzaSalami5">Pizza Salami 5€</option>
+            <option value="PizzaSchinken5">Pizza Schinken 5€</option>
+            <option value="PizzaHawaii6">Pizza Hawaii 6€</option>
+            <option value="PizzaSchinkenPilze6">Pizza Schinken und Pilze 6€</option>
+            <option value="PizzaMargherita4">Pizza Margeherita 4€</option>
         </select>
     </p>
+    <br>
 
     <h4>Zusatzwunsch auswählen:</h4>
-    <p><input type="checkbox" name="Oliven 1" value="Oliven">Oliven 1€</p>
-    <p><input type="checkbox" name="Mozerella 1" value="Mozerella">Mozerella 1€</p>
-    <p><input type="checkbox" name="Parma-Schinken 1" value="Parma-Schinken">Parma-Schinken 2€</p>
+    <p><input type="checkbox" name="Oliven1" value="Oliven">Oliven 1€</p>
+    <p><input type="checkbox" name="Mozzarella1" value="Mozerella">Mozerella 1€</p>
+    <p><input type="checkbox" name="Parma-Schinken2" value="Parma-Schinken">Parma-Schinken 2€</p>
 
+    <p><input type="reset" value="Reset">
+        <input type="submit" value="Submit"></p>
 </form>
-<p><input type="submit"> <input type="reset"></p>
-</form>
+
 </body>
 </html>
--- Aufgaben/Aufgabe4_Ausgabe.php
+++ Aufgaben/Aufgabe4_Ausgabe.php
@@ -3,5 +3,4 @@ echo "<h2>Bestellbestätigung:</h2>";
 echo "Hallo Herr/Frau".$_POST["Name"].", ihre Bestellung war erfolgreich. Sie haben bestellt: <br>";
 echo "Eine  ";
 
-
 ?>
